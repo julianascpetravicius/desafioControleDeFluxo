b39d05079e0925d8d87dfe0374918f4cc0043619
Juliana Petravicius <135447143+julianascpetravicius@users.noreply.github.com>
2024-05-29 12:39:49 +0100

    first commit

---
+++ src/main/java/Contador.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = terminal.nextInt();
+
+        try {
+
+            contar(parametroUm, parametroDois);
+
+            int numeroInicial = Math.min(parametroUm, parametroDois);
+            int numeroFinal = Math.max(parametroUm, parametroDois);
+            int contagem = numeroFinal - numeroInicial + 1;
+
+            System.out.println("Intervalo de números:");
+
+            for (int i = numeroInicial; i <= numeroFinal; i++) {
+                System.out.println("Imprimirndo numero " + i);
+            }
+
+        }catch (ParametrosInvalidosException e) {
+            System.err.println( "O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+    }
+    static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+        if (parametroUm >= parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
+        int contagem = parametroDois - parametroUm;
+
+        for (int i = 0; i < contagem; i++) {
+            System.out.println(i);
+        }
+        //realizar o for para imprimir os números com base na variável contagem
+    }
+}
---
+++ src/main/java/ParametrosInvalidosException.java
@@ -0,0 +1,5 @@
+public class ParametrosInvalidosException extends Throwable {
+    public static void main(String[] args) {
+
+    }
+}
